3f9655334f8ce7b831a79030bea5383d94ab07fb
chad_erdwins <cerdwins7@gmail.com>
2019-01-20 09:29:35 -0600

    finished problem 1.1, check if a string is unique

---
+++ Python/isUnique.py
@@ -0,0 +1,40 @@
+import unittest
+#brute force implementation
+# def unique(x):
+#     d = {}
+#     for char in x:
+#         if char in d.keys():
+#             return False
+#         else:
+#             d[char] = 1
+#     for value in d.values():
+#         if value > 1:
+#             return False
+#     return True
+
+#optimized version
+def unique(x):
+    d = {}
+    for char in x:
+        if char in d:
+            return False
+        else:
+            d[char] = 1
+    return True
+
+class Test(unittest.TestCase):
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+    def test_unique(self):
+        # true check
+        for test_string in self.dataT:
+            actual = unique(test_string)
+            self.assertTrue(actual)
+        # false check
+        for test_string in self.dataF:
+            actual = unique(test_string)
+            self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()
