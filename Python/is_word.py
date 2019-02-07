e38c3b25a730a179d2dd0486b23712602ba6b41b
chad_erdwins <cerdwins7@gmail.com>
2019-02-07 17:45:41 -0600

    solved a problem I missed on an interview

--- Python/is_word.py
+++ Python/is_word.py
@@ -1,4 +1,22 @@
-file = open("dictionary.txt")
+def is_word(string):
+    perms = permutations(string)
+    with open('dictionary.txt', 'r') as file:
+        li = file.readlines()
+        for item in perms:
+            if item + '\n' in li:
+                print(item)
+    return False
 
-li = file.readlines()
-print(len(li))
+
+def permutations(word):
+    if len(word) == 1:
+        return [word]
+    perms = permutations(word[1:])
+    char = word[0]
+    result = []
+    for perm in perms:
+        for i in range(len(perm) + 1):
+            result.append(perm[:i]+char+perm[i:])
+    return result
+
+print(is_word('race'))
